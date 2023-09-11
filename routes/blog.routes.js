a2b4069225f90edac21c12c77fce3e483618b8b6
Lautaro Cabrera <lau.cabrera114@gmail.com>
2023-09-11 19:07:03 -0300

    Primer commit

---
+++ routes/blog.routes.js
@@ -0,0 +1,41 @@
+// Se implementa el enrutador de Express
+
+const { Router } = require("express");
+const router = Router();
+const {
+  actualizarPublicacion,
+  crearPublicacion,
+  eliminarPublicacion,
+  obtenerPublicaciones,
+} = require("../controllers/blog.controller");
+
+// ==================================================
+//         Rutas para renderizar vistas
+// ==================================================
+
+router.get("/", (req, res) => {
+  res.render("home");
+});
+
+// Ruta para devolver la vista admin
+router.get("/admin", (req, res) => {
+  res.render("admin");
+});
+
+// ==================================================
+//         Rutas para CRUD de Publicaciones
+// ==================================================
+
+// Crear nueva publicación
+router.post("/publicacion", crearPublicacion);
+
+// Obtener todas las publicaciones
+router.get("/publicaciones", obtenerPublicaciones);
+
+// Actualizar una publicación
+router.put("/publicacion/:id", actualizarPublicacion);
+
+// Eliminar una publicación
+router.delete("/publicacion/:id", eliminarPublicacion);
+
+module.exports = router;
